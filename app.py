1947f1c09fad91e2d978dc777982f8b42de7a0ff
MahaAli18 <43891334+MahaAli18@users.noreply.github.com>
2020-07-09 22:03:48 +0500

    spelling change

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-db_uri = 'mysql+pymysql://root:pass@localhost:3306/users'
+db_uri = 'mysql+pymysql://root:pass@localhost:5000/users'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
