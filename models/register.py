eb356ac948de9bff98b79e2293ef43dda7f59072
MubashiraSalman <mubashirasalman6@gmail.com>
2020-07-27 00:41:27 +0500

    validation and storing picture

--- models/register.py
+++ models/register.py
@@ -5,11 +5,11 @@ class Register(db.Model):
     idd = db.Column(db.Integer, primary_key=True, nullable=False)
     email = db.Column(db.Text, nullable=False)
     password = db.Column(db.Text, nullable=False)
-    photo = db.Column(db.BLOB, nullable=False)
+    photo = db.Column(db.BLOB, nullable=True)
 
     @classmethod
-    def insert_in_db(cls, email, password):
-        reg = Register(email=email, password=password)
+    def insert_in_db(cls, email, password, img):
+        reg = Register(email=email, password=password, photo=img)
         db.session.add(reg)
         db.session.commit()
         return reg
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,5 @@
-from flask import abort, redirect, request, render_template
+from flask import Flask, abort, redirect, request, render_template, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import app
 from models.register import Register
 from db import db
@@ -14,12 +15,44 @@ def index_get2():
     return render_template('index.html')
 
 
+@app.route('/login.html', methods=['POST'])
+def signup():
+    email = request.form['email']
+    password = request.form['password']
+    file = request.files['filename']
+    new_file = file.read()
+
+    user = Register.query.filter_by(email=email).first()  # if this returns a user, then the email already exists in database
+
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect('/index.html')
+
+    Register.insert_in_db(email, password, new_file)
+    return render_template('/login.html')
+
+
 @app.route('/login.html', methods=['GET'])
 def login_get():
     return render_template('login.html')
 
 
 @app.route('/test.html', methods=['POST'])
+def login_post():
+    email = request.form['email']
+    password = request.form['password']
+
+    user = Register.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not (user.password == password):
+        return redirect('/login.html')  # if the user doesn't exist or password is wrong, reload the page
+    else:
+        return redirect('/test.html')
+
+
+@app.route('/test.html', methods=['GET'])
 def start_test():
-    return render_template('test.html')
+    return render_template('/test.html')
+
 
